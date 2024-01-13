c0475a88e5b4f942d29c5ba7dd86e4bc66e9f7ea
Vladis Markin <vladismarkin@gmail.com>
2024-01-13 19:29:57 +0200

    change folder name

--- src/components/subscription-form/subscription-form.tsx
+++ src/components/subscription-form/subscription-form.tsx
@@ -3,10 +3,10 @@ import React, { useCallback, useContext } from "react";
 
 import { Organization, User } from "../../types";
 import Loader from "../shered-components/loader/loader";
-import UsersDropdown from "./dropdown/users-drop-down/users-drop-down";
+import UsersDropdown from "./dropdowns/users-drop-down/users-drop-down";
 import { OrganizationContext } from "../../context/organizations";
 import ErrorMessage from "../shered-components/error-message/error-message";
-import OrganizationsDropdown from "./dropdown/organization-drop-down/organization-drop-down";
+import OrganizationsDropdown from "./dropdowns/organization-drop-down/organization-drop-down";
 import styles from "./subscription-form.module.css";
 import Button from "../shered-components/button/button";
 
